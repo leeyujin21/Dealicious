3ed7d00345255233e73ee4e6aa9e9cde309c7fd2
lubby <ch2488@naver.com>
2023-11-21 20:07:18 +0900

    1121 포크된 프로젝트 생성 하고 메인리포지토리로 옮기기

---
+++ Delicious/src/test/java/com/kosta/delicious/DeliciousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosta.delicious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliciousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
