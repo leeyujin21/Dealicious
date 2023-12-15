a547846367bf1000cab4f807474bf527bd17e607
leeyujin21 <dkdk506@naver.com>
2023-12-15 16:10:06 +0900

    20231215_lyj_03

--- Back/dealicious/src/main/java/com/kosta/deal/repository/CorpDataRepository.java
+++ Back/dealicious/src/main/java/com/kosta/deal/repository/CorpDataRepository.java
@@ -5,9 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.kosta.deal.entity.CorpData;
 
 public interface CorpDataRepository extends JpaRepository<CorpData, Integer> {
-<<<<<<< Updated upstream
 	
-=======
-
->>>>>>> Stashed changes
 }
--- Back/dealicious/src/main/java/com/kosta/deal/repository/SaleDslRepository.java
+++ Back/dealicious/src/main/java/com/kosta/deal/repository/SaleDslRepository.java
@@ -6,15 +6,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Order;
-import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Repository;
 
 import com.kosta.deal.entity.QSale;
-import com.kosta.deal.entity.QSaleLike;
 import com.kosta.deal.entity.QUser;
 import com.kosta.deal.entity.Sale;
-import com.kosta.deal.entity.SaleLike;
 import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
@@ -43,11 +39,6 @@ public class SaleDslRepository {
 				.where(sale.category.eq(category)).fetch();
 	}
 
-	public Sale findSaleBySaleNum(Integer num) throws Exception{
-		QSale sale=QSale.sale;
-		return jpaQueryFactory.selectFrom(sale)
-				.where(sale.num.eq(num)).fetchOne();
-	}
 
 	 public Long findSaleCount() throws Exception{
 		 
@@ -57,9 +48,13 @@ public class SaleDslRepository {
 	                
 	    }
 
-	
+	public Sale findSaleBySaleNum(Integer num) {
+		QSale sale=QSale.sale;
+		return jpaQueryFactory.selectFrom(sale)
+				.where(sale.num.eq(num)).fetchOne();
+	}
 
-	public Tuple findUserEmailAndRolesBySaleNum(Integer num)throws Exception {
+	public Tuple findUserEmailAndRolesBySaleNum(Integer num) {
 		QSale sale= QSale.sale;
 		QUser user= QUser.user;
 		return jpaQueryFactory.select(sale,user.nickname,user.typename,user.profileimgurl)
@@ -69,36 +64,6 @@ public class SaleDslRepository {
 				.where(sale.num.eq(num))
 				.fetchOne();
 	}
-
-	public Long findIsSalelike(String email, Integer num) throws Exception{
-		QSaleLike salelike= QSaleLike.saleLike;
-		
-		return jpaQueryFactory.select(salelike.count())
-				.from(salelike)
-				.where(salelike.userEmail.eq(email).and(salelike.num.eq(num)))
-				.fetchOne();
-	}
-
-
-	public SaleLike findSalelike(String email, Integer num) {
-		QSaleLike salelike= QSaleLike.saleLike;
-		return jpaQueryFactory.select(salelike)
-				.from(salelike)
-				.where(salelike.userEmail.eq(email).and(salelike.saleNum.eq(num)))
-				.fetchOne();
-	}
-
-	public Sale findByemail(String email, Integer num) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	
-
-	
-
-	
 
 	
 
