2efc64d847381a38488b8a57b60cd8139f789df3
leeyujin21 <dkdk506@naver.com>
2023-12-18 11:57:23 +0900

    20231218_lyj_01

--- Front/dealicious/src/component/SaleList.js
+++ Front/dealicious/src/component/SaleList.js
@@ -94,7 +94,7 @@ const SaleList=()=> {
             <div style={{ height: "35px", display: "flex" }} >
 
               {item.fileurl==null ?<img src='./profile.png' width="130px" height="87px"/> 
-              :<img src={`http://localhost:8090/img/${item.fileurl}`} width="130px" height="87px" />}
+              :<img src={`http://localhost:8090/img/${item.fileurl[0]}${item.fileurl[1]}`} width="130px" height="87px" />}
   
               <div style={{ textAlign: "left", marginLeft: "20px" }}>
                 <a style={{ fontSize: "18px" }}>{item.title}</a>
